2a14eeca12c6ad564f9efe339d9869b1b507eb4c
Shiiiba5794 <xuanbac5794@gmail.com>
2025-09-21 12:41:56 +0700

    Update Introduction.java

---
+++ HW1/Introduction/src/Introduction.java
@@ -0,0 +1,13 @@
+public class Introduction {
+    public static void main(String[] args) {
+        System.out.println("Phạm Xuân Bắc\t"+24020037+"\tK69I-IT1\t"+"Shiiiba5794\t"+"24020037@vnu.edu.vn");
+        System.out.println("9 bottles of beer on the wall, 9 bottles of beer.");
+        System.out.println("Take one down, pass it around, ");
+        System.out.println("8 bottles of beer on the wall, 8 bottles of beer.");
+        System.out.println("Take one down, pass it around,");
+        System.out.println("...");
+        System.out.println("1 bottle of beer on the wall, 1 bottle of beer.");
+        System.out.println("Take one down, pass it around,");
+        System.out.println("No more bottles of beer on the wall.");
+    }
+}
