dec077d16d1e98e9c4a16776deb4f83dffecc907
Shiiiba5794 <xuanbac5794@gmail.com>
2025-09-23 14:00:41 +0700

    Addtional exercises

--- HW1/Introduction.java
+++ HW1/Introduction.java
@@ -1,13 +1,10 @@
 public class Introduction {
     public static void main(String[] args) {
-        System.out.println("Phạm Xuân Bắc\t"+24020037+"\tK69I-IT1\t"+"Shiiiba5794\t"+"24020037@vnu.edu.vn");
-        System.out.println("9 bottles of beer on the wall, 9 bottles of beer.");
-        System.out.println("Take one down, pass it around, ");
-        System.out.println("8 bottles of beer on the wall, 8 bottles of beer.");
-        System.out.println("Take one down, pass it around,");
-        System.out.println("...");
-        System.out.println("1 bottle of beer on the wall, 1 bottle of beer.");
-        System.out.println("Take one down, pass it around,");
+        System.out.println("Phạm Xuân Bắc\t" + 24020037 + "\tK69I-IT1\t" + "Shiiiba5794\t" + "24020037@vnu.edu.vn");
+        for (int i = 9; i > 0; i--) {
+            System.out.println(i + " bottles of beer on the wall, " + i + " bottles of beer.");
+            System.out.println("Take one down, pass it around, ");
+        }
         System.out.println("No more bottles of beer on the wall.");
     }
 }
